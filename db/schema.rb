fdd697be2ff8f0cc4c5953e778247b4caa2e5fd7
Matt Gardner <mgardner@MAPC-MacPro.local>
2015-12-16 11:07:36 -0500

    Trying to apply changes again to develop. Contributor presenter includes name formatting helpers. Javascript triggers modals. User includes dummy avatar method.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215205923) do
+ActiveRecord::Schema.define(version: 20151215292904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -190,6 +190,8 @@ ActiveRecord::Schema.define(version: 20151215205923) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "organization_id"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
