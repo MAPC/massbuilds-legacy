c238cbabcd4c0e2e29d515146d23b9d46405364c
Matt Gardner <mgardner@MAPC-MacPro.local>
2015-12-16 11:15:41 -0500

    Fixed stray tabs.

--- app/presenters/contributor_presenter.rb
+++ app/presenters/contributor_presenter.rb
@@ -1,14 +1,14 @@
 class ContributorPresenter < Burgundy::Item
   def self.first_name
-  	first_name.titleize
+    first_name.titleize
   end
 
   def self.last_name
-  	last_name.titleize
+    last_name.titleize
   end
 
   def short_name
-  	"#{first_name} #{last_name.chars.first}."
+    "#{first_name} #{last_name.chars.first}."
   end
 
   def full_name
