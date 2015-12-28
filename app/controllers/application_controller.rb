d24b4c0504c9d781227764190332653b32ebe92f
Matt Gardner <wilburnforce@gmail.com>
2015-12-28 14:43:12 -0500

    Some cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,7 @@ class ApplicationController < ActionController::Base
     if user_signed_in?
       super
     else
-      # flash[:danger] = exception.message
       redirect_to '/users/sign_in', :notice => 'if you want to add a notice'
-      ## if you want render 404 page
-      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
     end
   end
 end
--- test/features/edit_organization_test.rb
+++ test/features/edit_organization_test.rb
@@ -2,15 +2,16 @@ require "test_helper"
 
 class EditOrganizationTest < Capybara::Rails::TestCase
 
-  def organization
-    @organization ||= organizations :mapc
-  end
-  alias_method :org, :organization
-
   def setup
     @user = users :normal
     @user.password = 'password'
   end
+
+  def organization
+    @organization ||= organizations :mapc
+  end
+
+  alias_method :org, :organization
 
   test "signed in user visits existing organization, and successfully edits it" do
     sign_in @user, visit: true, submit: true
