71c09c8f02e8dafa841e4b73fbe6d6a4ff766ed9
Matt Gardner <wilburnforce@gmail.com>
2015-12-29 14:18:38 -0500

    indentation. removed unnecessary presenter method.

--- app/presenters/organization_presenter.rb
+++ app/presenters/organization_presenter.rb
@@ -2,8 +2,4 @@ class OrganizationPresenter < Burgundy::Item
   def members
     UserPresenter.wrap item.members.sort_by(&:last_name)
   end
-
-  def developments
-    item.developments.uniq
-  end
 end
