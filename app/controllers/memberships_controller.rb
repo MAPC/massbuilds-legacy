c03ad77c0ecdce7b26e2bad0ac79a7a5a34a11d9
Matt Gardner <wilburnforce@gmail.com>
2015-12-31 12:54:23 -0500

    Cosmetics.

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -14,25 +14,13 @@ class MembershipsController < ApplicationController
   end
 
   def deactivate
-    @membership = Membership.find(membership_params[:membership_id])
-    @membership.deactivated.save
+    membership = Membership.find(membership_params[:membership_id])
+    membership.deactivated.save
     redirect_to current_user
-  end
-
-  def activate
-    @membership = Membership.new(user: current_user)
-
-    if @membership.save
-      flash[:success] = "Membership request sent."
-      redirect_to @membership
-    else
-      flash[:danger] = @membership.errors.full_messages
-      render new_organization_path
-    end
   end
 
   private
     def membership_params
-      params.permit(:membership_id, :user_id, :organization_id, :id, :_method, :authenticity_token,)
+      params.permit(:membership_id, :user_id, :organization_id, :id)
     end  
 end
