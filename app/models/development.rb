6f346197ecf02b368600e8a26ba58cfcf1aef608
Matt Cloyd <cloydster@gmail.com>
2016-01-29 16:55:51 -0500

    Remove searches controller. Clean up developments a tiny bit.

--- app/models/development.rb
+++ app/models/development.rb
@@ -1,26 +1,27 @@
 class Development < ActiveRecord::Base
-  extend Enumerize
-
-  before_save :update_tagline
-  before_save :clean_zip_code
-
   # TODO basic geocoding
   # geocoded_by :full_street_address   # Implement this method
   # after_validation :geocode          # Auto-fetch coordinates
-
-  belongs_to :creator, class_name: :User
   has_one :walkscore # TODO
 
+  extend Enumerize
+
+  # Callbacks
+  before_save :update_tagline
+  before_save :clean_zip_code
+
+  # Relationships
+  belongs_to :creator, class_name: :User
   has_many :edits
   has_many :flags
   has_many :crosswalks
   has_many :team_memberships, class_name: :DevelopmentTeamMembership,
-    counter_cache: :team_membership_count
+            counter_cache: :team_membership_count
   has_many :team_members, through: :team_memberships, source: :organization
-
   has_and_belongs_to_many :programs
 
-  validates :creator, presence: true
+  # Validations
+  validates :creator,    presence: true
   validates :year_compl, presence: true
 
   STATUSES = [:projected, :planning, :in_construction, :completed]
@@ -30,6 +31,7 @@ class Development < ActiveRecord::Base
   alias_attribute :zip, :zip_code
   alias_attribute :hidden, :private
 
+  # Scopes
   ranged_scopes :created_at, :updated_at, :height, :stories,
     :year_compl, :affordable, :prjarea, :singfamhu, :twnhsmmult,
     :lgmultifam, :tothu, :gqpop, :rptdemp, :emploss, :estemp, :commsf,
@@ -42,7 +44,7 @@ class Development < ActiveRecord::Base
   scope :close_to, CloseToQuery.new(self).scope
 
   def location
-    # [longitude, latitude]
+    # TODO [longitude, latitude]
   end
 
   def zip_code
@@ -86,9 +88,11 @@ class Development < ActiveRecord::Base
     def update_tagline
       self.tagline = TaglineGenerator.new(self).perform!
     end
+
     def clean_zip_code
       self.zip_code = self.zip_code.to_s.gsub(/\D*/, '')
     end
+
     def nine_digit_formatted_zip(code)
       "#{code[0..4]}-#{code[-4..-1]}"
     end
