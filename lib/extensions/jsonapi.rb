75093c87330c319813eb3dc82f1b3d2bae04e5e9
Matt Cloyd <cloydster@gmail.com>
2016-02-02 12:23:26 -0500

    Update JSONAPI extension to handle ranged and boolean filters.

--- lib/extensions/jsonapi.rb
+++ lib/extensions/jsonapi.rb
@@ -1,11 +1,34 @@
 module JSONAPI
-  class LinkBuilder
 
+  class Resource
+    class << self
+      def range_filters(*filter_attributes)
+        Array(filter_attributes).flatten.each {|attribute|
+          filter(attribute, apply: ->(records, value, options) {
+            records.where(attribute => RangeParser.parse(value))
+          })
+        }
+      end
+
+      def boolean_filters(*filter_attributes)
+        Array(filter_attributes).flatten.each {|attribute|
+          filter(attribute, apply: ->(records, value, options) {
+            records.where(attribute => value)
+          })
+        }
+      end
+
+      alias_method :range_filter,   :range_filters
+      alias_method :boolean_filter, :boolean_filters
+    end
+  end
+
+
+  class LinkBuilder
     private
 
     def formatted_module_path_from_class(klass)
       '/'
     end
-
   end
 end
