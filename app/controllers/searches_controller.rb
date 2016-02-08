7acc72fa910b6832d2b91c0882a9451d38a8dcd4
Matt Cloyd <cloydster@gmail.com>
2016-02-08 13:51:53 -0500

    Set up CSV exports of search results.
    Why:
    
    Users should be able to export the results of their searches as CSV.
    Similar to PDF reports, when a user exports, the search is added to
    their saved searches, and the data is sent.
    
    This change addresses the need by:
    
    * Adding a CSV format to searches/show that sends CSV data.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,13 +4,25 @@ class SearchesController < ApplicationController
   before_action :ensure_saved_search
 
   def show
-    render pdf: "#{@search.id}",
-         title: "#{@search.title}",
-         show_as_html: params[:format] == 'html',
-             template: 'searches/show.html.haml'
+    respond_to do |format|
+      format.pdf  { render pdf_options }
+      format.html { render pdf_options.merge({ show_as_html: true }) }
+      format.csv  { send_data @search.to_csv, type: Mime::CSV,
+        disposition: disposition }
+    end
   end
 
   private
+
+  def pdf_options
+    { pdf:   @search.id.to_s,
+      title: @search.title.to_s,
+      template: 'searches/show.html.haml' }
+  end
+
+  def disposition
+    "attachment; filename=#{@search.title.parameterize}.csv"
+  end
 
   def load_record
     @search = ReportPresenter.new(Search.find(params[:id]))
