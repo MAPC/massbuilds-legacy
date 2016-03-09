5c01e53c92666266ff032434f21425fce950bc17
Matt Cloyd <cloydster@gmail.com>
2016-03-09 09:58:31 -0500

    Fix slow-running test failure.
    Why:
    
    A test was failing because, as the test suite grew, it took longer from
    record creation to get to the test, so the time_ago_in_words helper
    would generate '2 minutes ago' instead of the expected 'less than a
    minute ago' or '1 minute ago'.
    
    This change addresses the need by:
    
    * Splitting two test cases that were lumped together in one test into
    two tests.
    
    * Creating the record at test time, so we can expect the test will
    always return 'less than a minute ago' no matter how long the tests take
    to run.

--- test/presenters/edit_presenter_test.rb
+++ test/presenters/edit_presenter_test.rb
@@ -21,14 +21,18 @@ class EditPresenterTest < ActiveSupport::TestCase
     assert_respond_to pres, :changes
   end
 
-  test '#time_ago' do
+  test '#time_ago when applied' do
     item.applied_at = 10.hours.ago
     item.applied = true
     assert_equal 'about 10 hours ago', pres.time_ago
-    item.applied = false
-    # Options are for slow-running tests on Travis CI
-    time_options = ['less than a minute ago', '1 minute ago']
-    assert_includes time_options, pres.time_ago
+  end
+
+  test '#time_ago when not applied' do
+    expected = 'less than a minute ago'
+    edit = Edit.new
+    edit.save(validate: false)
+    actual = EditPresenter.new(edit).time_ago
+    assert_equal expected, actual
   end
 
   test '#time is an alias of #time_ago' do
