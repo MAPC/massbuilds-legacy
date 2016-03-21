48e02c7e73875ba9ae760c07a99effe11b5ac091
Matt Cloyd <cloydster@gmail.com>
2016-03-21 19:00:01 -0400

    Add email frequency preference
    Why:
    
    Users should be able to determine how frequently they get development
    updates.
    
    This change addresses the need by:
    
    * Add a link to edit the user profile at the bottom of digest emails
    
    * Add radio buttons to the form to let the user set their mail
    frequency.
    
    * Allow the mail_frequency attribute through the controller, and set the
    user's profile as the route to go to after updating.
    
    * Fixes a bug in mail_frequency checking, but I introduce a Nil Check,
    so I'm not proud of the fix.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,8 +11,13 @@ class RegistrationsController < Devise::RegistrationsController
       :password, :password_confirmation)
   end
 
-  def account_update_params
+  def account_params
     params.require(:user).permit(:first_name, :last_name, :email,
-      :password, :password_confirmation, :current_password)
+      :password, :password_confirmation, :current_password,
+      :mail_frequency)
+  end
+
+  def after_update_path_for(resource)
+    user_path(resource)
   end
 end
