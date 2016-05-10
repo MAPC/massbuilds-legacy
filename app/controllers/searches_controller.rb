e13cb00185c885c1375620ffb810abec160c6c02
Matt Cloyd <cloydster@gmail.com>
2016-05-10 15:23:43 -0400

    Refactor SearchesController
    Why:
    
    I'm planning on addressing some controller-level issues shortly, but I
    noticed a lot of misplaced code at the bottom of the controller.
    
    PDF and CSV configuration options belong in their own classes.
    
    This change addresses the need by:
    
    * Creating an Export module for different formats, then adding a CSV and
    PDF class inside the module.
    
    * Instantiating Export classes in the Searches Controller.
    
    * Adding 'CSV' and 'PDF' as acronyms for Rails to be aware of, so it
    doesn't look for 'Csv' and 'Pdf'.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,40 +5,19 @@ class SearchesController < ApplicationController
 
   def show
     respond_to do |format|
-      format.pdf  { render pdf_options }
-      format.html { render pdf_options.merge({ show_as_html: true }) }
-      format.csv  {
-        send_data @search.to_csv, type: Mime::CSV, disposition: disposition
-      }
+      format.pdf  { render Export::PDF.new(@search).render }
+      format.html { render Export::PDF.new(@search, show_as_html: true).render }
+      format.csv  { send_data *Export::CSV.new(@search).render }
     end
   end
 
   private
 
-  def pdf_options
-    {
-      pdf:   @search.id.to_s,
-      title: @search.title.to_s,
-      layout: 'pdf',
-      template: 'searches/show.html.haml',
-      header: { right: '[page] of [topage]', font_size: 9 },
-      footer: {
-        # TODO: MAPC Logo
-        left: "Generated on #{Time.now.to_s(:timestamp)}",
-        right: 'mapc.org',
-        font_size: 9
-      }
-    }
-  end
-
-  def disposition
-    "attachment; filename=#{@search.title.parameterize}.csv"
-  end
-
   def load_record
     @search = ReportPresenter.new(Search.find(params[:id]))
   end
 
+  # TODO: Refactor
   def ensure_saved_search
     search = @search.item
     if search.unsaved?
---
+++ app/models/export/pdf.rb
@@ -0,0 +1,30 @@
+class Export::PDF
+
+  def initialize(record, filename: :id, title: :title, **options)
+    @record   = record
+    @filename = @record.send(filename).to_s
+    @title    = @record.send(title).to_s
+    @config   = default_config.merge(options)
+  end
+
+  def render
+    @config
+  end
+
+  def default_config
+    {
+      pdf:      @filename,
+      title:    @title,
+      layout:   'pdf',
+      template: 'searches/show.html.haml',
+      header: { right: '[page] of [topage]', font_size: 9 },
+      footer: {
+        # TODO: MAPC Logo
+        left:  "Generated on #{Time.now.to_s(:timestamp)}",
+        right: 'mapc.org',
+        font_size: 9
+      }
+    }
+  end
+
+end
