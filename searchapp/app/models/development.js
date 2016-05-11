c8c20fb8226d6fbc58b11ff23b04a24c859e0f1c
Matt Gardner <wilburnforce@gmail.com>
2016-05-11 14:17:25 -0400

    Fixed location bug.

--- searchapp/app/models/development.js
+++ searchapp/app/models/development.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   latitude: DS.attr("number"),
@@ -6,8 +7,13 @@ export default DS.Model.extend({
   acres: DS.attr("number"),
   name: DS.attr("string"),
   city: DS.attr("string"),
-  latitude: DS.attr("number"),
-  longitude: DS.attr("number"),
+  latitude: Ember.computed('location', function() {
+    return this.get("location")[0]
+  }),
+  longitude: Ember.computed('location', function() {
+    return this.get("location")[1]
+  }),
+  location: DS.attr(),
   commsf: DS.attr("string"),
   tothu: DS.attr("number"),
   year: DS.attr("number"),
