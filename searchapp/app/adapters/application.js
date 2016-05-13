c013b126bbb435089f6e057fc4fade1aa3295847
Matt Gardner <wilburnforce@gmail.com>
2016-05-13 11:23:12 -0400

    remove console log

--- searchapp/app/adapters/application.js
+++ searchapp/app/adapters/application.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
-console.log(window.location);
+
 export default DS.JSONAPIAdapter.extend({
   host: '//api.' + window.location.host,
   headers: Ember.computed(function() {
