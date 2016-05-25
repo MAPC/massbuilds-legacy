39332ed3fcd49e48efcbdd7f678ff3f8b0829735
Matt Cloyd <cloydster@gmail.com>
2016-05-25 08:39:00 -0400

    Paginate organizations
    Why:
    
    There are about 600 organizations. We need to paginate them.
    
    This change addresses the need by:
    
    * Paginating organizations.
    
    * Generating Semantic UI / HAML views for pagination.
    
    * Adding organization fixture for MAPC.

---
+++ db/fixtures/organizations.rb
@@ -0,0 +1,14 @@
+Organization.seed(:id,
+  {
+    id:          1,
+    name:       'Metropolitan Area Planning Council',
+    website:    'mapc.org',
+    location:   'Boston, MA',
+    email:      'info@mapc.org',
+    phone:      '617 933 0070',
+    address:    '60 Temple Place, Boston MA 02111',
+    short_name: 'MAPC',
+    abbv:       'MAPC',
+    municipal:  false
+  }
+)
