b847080f11a2e9bbbc7f0b428a644b65c77d03f9
Matt Cloyd <cloydster@gmail.com>
2016-06-02 17:18:24 -0400

    Fix comma typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
     address:         ENV['MAILGUN_SMTP_SERVER'],
     user_name:       ENV['MAILGUN_SMTP_LOGIN'],
     password:        ENV['MAILGUN_SMTP_PASSWORD'],
-    domain:          ENV['MAILGUN_DOMAIN']
+    domain:          ENV['MAILGUN_DOMAIN'],
     authentication:  :plain
   }
 
