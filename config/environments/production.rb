4e64106cec799789cc6eaba13978ff2eacc57876
Matt Cloyd <cloydster@gmail.com>
2016-06-03 12:45:59 -0400

    Remove 'authentication: :plain' from production mail settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,8 +83,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key:        ENV['MAILGUN_API_KEY'],
-    domain:         ENV['MAILGUN_DOMAIN'],
-    authentication: :plain
+    domain:         ENV['MAILGUN_DOMAIN']
   }
 
   config.action_mailer.default_url_options = { host: DEFAULT_HOST }
