af2580e808ed6706d619696e6c3607b0e704ead7
Matt Cloyd <cloydster@gmail.com>
2016-06-03 12:08:12 -0400

    Chamge mail method to Mailgun API
    Why:
    
    Sending email with Mailgun and SMTP was causing errors. Decided to
    switch to the Mailgun HTTP API.
    
    This change addresses the need by:
    
    * Adding mailgun_rails and connecting the API information.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,14 +80,11 @@ Rails.application.configure do
   DEFAULT_HOST = ENV.fetch 'DEFAULT_HOST', 'dd.mapc.org'
   Rails.application.default_url_options[:host] = DEFAULT_HOST
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    port:            ENV['MAILGUN_SMTP_PORT'],
-    address:         ENV['MAILGUN_SMTP_SERVER'],
-    user_name:       ENV['MAILGUN_SMTP_LOGIN'],
-    password:        ENV['MAILGUN_SMTP_PASSWORD'],
-    domain:          ENV['MAILGUN_DOMAIN'],
-    authentication:  :plain
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+    api_key:        ENV['MAILGUN_API_KEY'],
+    domain:         ENV['MAILGUN_DOMAIN'],
+    authentication: :plain
   }
 
   config.action_mailer.default_url_options = { host: DEFAULT_HOST }
