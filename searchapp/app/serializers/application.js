3d661b6f1f00e2409bd1d356dbf272bcd54cbebe
Matt Gardner <wilburnforce@gmail.com>
2016-06-17 14:52:49 -0400

    Fixing a deprecated API issue.

---
+++ searchapp/app/serializers/application.js
@@ -0,0 +1,11 @@
+// import App from '../app';
+// import { ActiveModelSerializer } from 'active-model-adapter';
+
+// App.storeMeta = {};
+// console.log("Called");
+// export default ActiveModelSerializer.extend({
+//   normalizeResponse(store, primaryModelClass, payload) {
+//     App.storeMeta['development'] = payload.meta; //ember data only allows meta data on 'query', this adds support for all other methods
+//     return this._super(...arguments);
+//   }
+// });
