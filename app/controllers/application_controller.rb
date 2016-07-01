eeb419b9e4be0c926be07c58ac304b0176aad7a1
Matt Cloyd <cloydster@gmail.com>
2016-07-01 12:27:32 -0400

    Update login form
    Why:
    
    Want it to look a little cleaner, more like your standard login page.
    
    This change addresses the need by:
    
    * Following the style of the Semantic UI login example:
        http://semantic-ui.com/examples/login.html
    
    * Added a Devise layout.
    
    * Added a conditional to use Devise layout for Devise controller
    actions.
    
    * Updated the prompt to sign in.
    
    NOTE: Had trouble implementing client-side validation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  layout :layout_by_resource
+
   # Send 'em back where they came from with a slap on the wrist
   def authority_forbidden(error)
     Authority.logger.warn(error.message)
@@ -16,6 +18,14 @@ class ApplicationController < ActionController::Base
   end
 
   protected
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
 
   def authenticate_user!(options = {})
     if user_signed_in?
