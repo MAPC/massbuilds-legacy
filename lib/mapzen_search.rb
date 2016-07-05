b48b97290249131a00c57b26723f7b1395ac0acd
Matt Cloyd <cloydster@gmail.com>
2016-07-05 13:35:42 -0400

    Add geocoding (address) search to searchables
    Why:
    
    The frontend Google geocoder/autocomplete wasn't working well with
    Ember, so we decided to include the search in the backend, and serving
    it out as a simple result.
    
    This change addresses the need by:
    
    * Creating a MapzenSearch service object, which looks up text using
    Mapzen autocomplete.
    
    * Adding the location to the top of the results,
    
    NOTE: This may need significant tuning. We should also focus the results
    dynamically when we have time to do so:
    https://trello.com/c/uUXb47Ah/153-focus-the-search.

---
+++ lib/mapzen_search.rb
@@ -0,0 +1,46 @@
+class MapzenSearch
+
+  attr_reader :id
+
+  def initialize(text)
+    @text = text
+    @url  = base + text
+  end
+
+  def id
+    @text
+  end
+
+  def geometry
+    {
+      type:        result['type'],
+      coordinates: result['geometry'],
+      properties:  result['properties']
+    }
+  end
+
+  def result
+    results.first
+  end
+
+  private
+
+  def results
+    JSON.parse(response)['features'].map { |json| OpenStruct.new(json) }
+  end
+
+  def response
+    Net::HTTP.get_response(URI(@url)).body
+  end
+
+  def base
+    @base ||= "http://search.mapzen.com/v1/autocomplete"
+    @base +=  "?api_key=#{ENV['MAPZEN_API_KEY']}"
+    @base +=  "&focus.point.lat=42.357&focus.point.lon=-71.056"
+    @base +=  "&sources=openstreetmap"
+    @base +=  "&layers=address,microhood,neighbourhood,macrohood"
+    @base +=  "&text="
+    @base.freeze
+  end
+
+end
