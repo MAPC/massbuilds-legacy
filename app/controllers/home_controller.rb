87242b14a960e72b783c29efa88ea4fcd1d1ca19
Matt Cloyd <cloydster@gmail.com>
2016-07-06 11:32:12 -0400

    Even though we're rendering root -> the High Voltage home page directly, it may be useful, once we can figure out routing, to add this conditional redirection back in.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     if signed_in?
       redirect_to :developments
     else
-      render 'index.html', layout: false
+      redirect_to page_path('home')
     end
   end
 
