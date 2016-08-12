b1d672ed1ec99b686ea1667c06d0ff19bea6d766
Matt Gardner <wilburnforce@gmail.com>
2016-08-12 12:38:29 -0400

    Improving development search by integrating Carto

--- searchapp/app/components/cartodb-layer.js
+++ searchapp/app/components/cartodb-layer.js
@@ -1,20 +1,39 @@
 import Ember from 'ember';
 import BaseLayer from 'ember-leaflet/components/base-layer';
+import squel from 'npm:squel';
 
 export default BaseLayer.extend({
+  actions: {
+    zoomToGeoJSON: function(geojson, type) {
+      this.recalculateBounds(geojson);
+    }
+  },
+
+  recalculateBounds(geojson) {
+    let geojsonLayer = L.geoJson(geojson),   
+      map = this.get('containerLayer')._layer,
+      bounds = geojsonLayer.getBounds();
+      // arraybounds = [[bounds.getWest(), bounds.getSouth()], [bounds.getEast(), bounds.getNorth()]];
+      console.log(geojson,bounds);
+      // arraybounds[0][1] += 0.000005;
+      // arraybounds[1][0] += 0.000005;
+
+    map.fitBounds(bounds);
+  },
   leafletRequiredOptions: [
     'url'
   ],
 
   leafletOptions: [
-    'zIndex', 'opacity'
+    'zIndex', 'opacity', 'zoomToGeoJSON'
   ],
 
   leafletEvents: [
+   'zoomToGeoJSON'
   ],
 
   leafletProperties: [
-    'url', 'zIndex', 'opacity'
+    'url', 'zIndex', 'opacity', 'zoomToGeoJSON'
   ],
 
   layerSetup() {
@@ -24,12 +43,27 @@ export default BaseLayer.extend({
     if (this.get('containerLayer')) {
       let map = this.get('containerLayer')._layer;
       let zIndex = this.get('options.zIndex');
+      let cartoSql = new cartodb.SQL({ user: 'mapc-maps' });
 
       this._layer.on('done', (layer) => {
         
+        var initial = squel.select()
+                      .from('developments')
+                      .field('cartodb_id')
+                      .field('ST_GeomFromEWKT(point)', 'the_geom')
+                      .field('ST_Transform(ST_GeomFromEWKT(point),3857)', 'the_geom_webmercator')
+                      .toString();
+
+        cartoSql.getBounds(initial).done((bounds) => {
+          console.log(bounds);
+          map.fitBounds(bounds);
+        });
+
         cdb.geo.LeafletMapView.addLayerToMap(layer, map, zIndex);
         this.layer = layer;
         this.didCreateLayer();
+
+
         if(this.get('sql')) {
           this.setSql();
         }
@@ -39,8 +73,15 @@ export default BaseLayer.extend({
   },
 
   setSql: function() {
+    let cartoSql = new cartodb.SQL({ user: 'mapc-maps' });
     let SQL = this.get('sql');
-    console.log(SQL);
+    let map = this.get('containerLayer')._layer;
+
+    cartoSql.getBounds(SQL).done((bounds) => {
+      console.log(bounds);
+      map.fitBounds(bounds);
+    });
+
     this.layer.getSubLayer(0).setSQL(SQL);
   }.observes('sql'),
 
