4cada10b269e6ae4a63c3503f11c3979f01749fc
Matt Cloyd <cloydster@gmail.com>
2016-09-09 12:04:10 -0400

    Redirect older browsers away from search, edit, and new.

--- config/initializers/browser.rb
+++ config/initializers/browser.rb
@@ -1,7 +1,10 @@
 Rails.configuration.middleware.use Browser::Middleware do
 
-  # TODO: Add developments#edit path to matchers.
-  if request.env['PATH_INFO'] == '/developments'
+  # Matches /developments, /developments/new, and /developments/:id/edit
+  EMBER_PATH_REGEX = /^\/developments\/?(new|\d+\/?edit|\/?$)/.freeze
+
+  # TODO: Add developments#edit and developments#new path to matchers.
+  if request.env['PATH_INFO'].match(EMBER_PATH_REGEX)
     # The "modern" detection allows IE9, but we can't support IE9.
     # So, if the "modern" check passes, we also need to check
     # If the browser is not modern, or it's IE 9, redirect the user.
